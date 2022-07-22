c38e8b937750f6950f769ec59671b0a79ba3704d
dciubak <dciubak@griddynamics.com>
2022-07-22 19:26:41 +0200

    Added the necessary @LayoutRes annotation to the BaseFragment class

--- presentation/src/main/java/com/example/presentation/BaseFragment.kt
+++ presentation/src/main/java/com/example/presentation/BaseFragment.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
-abstract class BaseFragment(private val fragmentRes: Int) : Fragment() {
+abstract class BaseFragment(@LayoutRes private val fragmentRes: Int) : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
