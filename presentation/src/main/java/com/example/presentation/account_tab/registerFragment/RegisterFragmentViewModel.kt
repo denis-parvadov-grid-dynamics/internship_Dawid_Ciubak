1cf5c39f2c945f09e65d7a53f34587d64718dec1
dciubak <dciubak@griddynamics.com>
2022-08-10 11:30:20 +0200

    removed db checks before loggin, handled internet connection errors when making api calls, created a visible loading bar while making api/db calls, password fields hide their content, added validation to the login/registration input fields

--- presentation/src/main/java/com/example/presentation/account_tab/registerFragment/RegisterFragmentViewModel.kt
+++ presentation/src/main/java/com/example/presentation/account_tab/registerFragment/RegisterFragmentViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.domain.model.User
 import com.example.domain.repository.AppDatabaseRepository
-import com.example.domain.repository.FakeStoreRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
