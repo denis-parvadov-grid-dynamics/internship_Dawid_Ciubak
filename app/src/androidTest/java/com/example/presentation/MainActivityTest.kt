b6ed1f135a3c41e478d47037bb0cf14f3b0e16e1
dciubak <dciubak@griddynamics.com>
2022-10-03 13:34:48 +0200

    Separated unit tests, added UI tests (fixed ktlint errors)

--- app/src/androidTest/java/com/example/presentation/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/presentation/MainActivityTest.kt
@@ -1,37 +1,26 @@
 package com.example.presentation
 
-
-import androidx.test.espresso.DataInteraction
-import androidx.test.espresso.ViewInteraction
-import androidx.test.filters.LargeTest
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
-
-import androidx.test.InstrumentationRegistry.getInstrumentation
-import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
-import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions.*
-import androidx.test.espresso.matcher.ViewMatchers.*
-
-import com.example.internshipappdawidciubak.R
-
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
 import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.Matchers.anything
-import org.hamcrest.Matchers.`is`
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -44,43 +33,58 @@ class MainActivityTest {
     @Test
     fun mainActivityTest() {
         val textInputEditText = onView(
-allOf(withId(androidx.appcompat.R.id.searchForItemsEditText),
-childAtPosition(
-childAtPosition(
-withId(androidx.appcompat.R.id.searchForItemsTextInputLayout),
-0),
-0),
-isDisplayed()))
+            allOf(
+                withId(R.id.searchForItemsEditText),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.searchForItemsTextInputLayout),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
         textInputEditText.perform(replaceText("mens"), closeSoftKeyboard())
-        
-        val editText = onView(
-allOf(withId(androidx.appcompat.R.id.searchForItemsEditText), withText("mens"),
-withParent(withParent(withId(androidx.appcompat.R.id.searchForItemsTextInputLayout))),
-isDisplayed()))
-        editText.check(matches(withText("mens")))
-        
-        val textView = onView(
-allOf(withId(android.R.id.text1), withText("Latest"),
-withParent(allOf(withId(androidx.appcompat.R.id.sortOrderSpinner),
-withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java)))),
-isDisplayed()))
-        textView.check(matches(withText("Latest")))
-        }
-    
-    private fun childAtPosition(
-            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
 
+        val editText = onView(
+            allOf(
+                withId(R.id.searchForItemsEditText), withText("mens"),
+                withParent(withParent(withId(R.id.searchForItemsTextInputLayout))),
+                isDisplayed()
+            )
+        )
+        editText.check(matches(withText("mens")))
+
+        val textView = onView(
+            allOf(
+                withId(android.R.id.text1), withText("Latest"),
+                withParent(
+                    allOf(
+                        withId(R.id.sortOrderSpinner),
+                        withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Latest")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>,
+        position: Int
+    ): Matcher<View> {
         return object : TypeSafeMatcher<View>() {
             override fun describeTo(description: Description) {
                 description.appendText("Child at position $position in parent ")
                 parentMatcher.describeTo(description)
             }
 
-            public override fun matchesSafely(view: View): Boolean {
+            override fun matchesSafely(view: View): Boolean {
                 val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
+                return (parent is ViewGroup) && (parentMatcher.matches(parent)) && (view == parent.getChildAt(position))
             }
         }
     }
-    }
+}
